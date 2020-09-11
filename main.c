71027e9c60602dee4597b217aa882ce455c72a88
developerspro <jeferson.oferreira@etec.sp.gov.br>
2020-09-11 14:39:33 -0300

    projeto inicial

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+main() {
+
+int A; 
+int B;
+int RESULTADO;
+printf("Digite um numero:\n");
+scanf("%d",&A);
+printf("Digite outro numero:\n");
+scanf("%d",&B);
+RESULTADO = A+B;
+printf("Resultado de %d + %d: %d",A,B,RESULTADO);
+
+}
